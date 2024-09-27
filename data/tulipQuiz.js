32efd7e38155740e20d83755af8135de1cc64bf5
Vachek-hooves <vachek0602@gmail.com>
2024-09-27 17:29:32 +0300

    create quiz data set

---
+++ data/tulipQuiz.js
@@ -0,0 +1,408 @@
+export const TulipQuiz = [
+  {
+    image: 'https://www.iamexpat.nl/sites/default/files/styles/ogimage_thumb/public/facts-dutch-netherlands.jpg',
+    id: 1,
+    isLocked: false,
+    title: "General Knowledge About the Netherlands",
+    questions: [
+      {
+        question: "What is the capital of the Netherlands?",
+        options: ["Amsterdam", "Rotterdam", "The Hague"],
+        answer: "Amsterdam"
+      },
+      {
+        question: "What is the official language of the Netherlands?",
+        options: ["Dutch", "German", "English"],
+        answer: "Dutch"
+      },
+      {
+        question: "What is the currency of the Netherlands?",
+        options: ["Dollar", "Euro", "Pound"],
+        answer: "Euro"
+      },
+      {
+        question: "What is the national flower of the Netherlands?",
+        options: ["Rose", "Tulip", "Lily"],
+        answer: "Tulip"
+      },
+      {
+        question: "What is the famous Dutch cheese?",
+        options: ["Cheddar", "Gouda", "Brie"],
+        answer: "Gouda"
+      },
+      {
+        question: "What is the traditional Dutch dish made from raw herring?",
+        options: ["Stroopwafel", "Haring", "Poffertjes"],
+        answer: "Haring"
+      },
+      {
+        question: "What is the national color of the Netherlands?",
+        options: ["Green", "Blue", "Orange"],
+        answer: "Orange"
+      },
+      {
+        question: "Which flower is most associated with the Netherlands?",
+        options: ["Orchid", "Daisy", "Tulip"],
+        answer: "Tulip"
+      },
+      {
+        question: "What famous artist is from the Netherlands?",
+        options: ["Picasso", "Van Gogh", "Monet"],
+        answer: "Van Gogh"
+      },
+      {
+        question: "What city is known for its canals?",
+        options: ["Berlin", "Amsterdam", "Paris"],
+        answer: "Amsterdam"
+      }
+    ]
+  },
+  {
+    image: 'https://www.thehagueinternationalcentre.nl/sites/default/files/styles/header_beeld_artikelen_/public/2018-10/historyklein.jpg?h=a0667207&itok=EImGThLp',
+    id: 2,
+    isLocked: true,
+    title: "History of the Netherlands",
+    questions: [
+      {
+        question: "When did the Netherlands gain independence?",
+        options: ["1581", "1600", "1700"],
+        answer: "1581"
+      },
+      {
+        question: "Who was the first King of the Netherlands?",
+        options: ["Willem I", "Willem II", "Willem III"],
+        answer: "Willem I"
+      },
+      {
+        question: "What major war involved the Netherlands in the 20th century?",
+        options: ["World War I", "World War II", "The Cold War"],
+        answer: "World War II"
+      },
+      {
+        question: "What historical event is known as the \"Dutch Golden Age\"?",
+        options: ["Industrial Revolution", "Economic prosperity in the 17th century", "The Renaissance"],
+        answer: "Economic prosperity in the 17th century"
+      },
+      {
+        question: "What was the main reason for the Dutch revolt against Spain?",
+        options: ["Taxation", "Religious freedom", "Trade disputes"],
+        answer: "Religious freedom"
+      },
+      {
+        question: "Who was the famous Dutch explorer who reached Australia?",
+        options: ["Marco Polo", "Abel Tasman", "Ferdinand Magellan"],
+        answer: "Abel Tasman"
+      },
+      {
+        question: "What is the name of the Dutch royal family?",
+        options: ["Habsburg", "Orange-Nassau", "Bourbon"],
+        answer: "Orange-Nassau"
+      },
+      {
+        question: "Which city was the capital of the Netherlands during the Napoleonic Wars?",
+        options: ["The Hague", "Amsterdam", "'s-Hertogenbosch"],
+        answer: "'s-Hertogenbosch"
+      },
+      {
+        question: "What treaty ended the Eighty Years' War?",
+        options: ["Treaty of Utrecht", "Treaty of Westphalia", "Treaty of Münster"],
+        answer: "Treaty of Münster"
+      },
+      {
+        question: "Which Dutch city was famous for its stock exchange in the 17th century?",
+        options: ["Rotterdam", "Amsterdam", "Utrecht"],
+        answer: "Amsterdam"
+      }
+    ]
+  },
+  {
+    image: 'https://upload.wikimedia.org/wikipedia/commons/0/05/Hollande04.jpg',
+    id: 3,
+    isLocked: true,
+    title: "Culture of the Netherlands",
+    questions: [
+      {
+        question: "What is the famous Dutch celebration in April?",
+        options: ["Sinterklaas", "King's Day", "Easter"],
+        answer: "King's Day"
+      },
+      {
+        question: "What is a traditional Dutch pastry?",
+        options: ["Croissant", "Stroopwafel", "Éclair"],
+        answer: "Stroopwafel"
+      },
+      {
+        question: "What type of painting is the Netherlands famous for?",
+        options: ["Abstract", "Impressionism", "Realism"],
+        answer: "Realism"
+      },
+      {
+        question: "What is the traditional Dutch drink?",
+        options: ["Beer", "Wine", "Whiskey"],
+        answer: "Beer"
+      },
+      {
+        question: "What kind of music is popular in the Netherlands?",
+        options: ["Classical", "Electronic", "Jazz"],
+        answer: "Electronic"
+      },
+      {
+        question: "What is the most famous Dutch festival?",
+        options: ["Carnival", "King's Day", "Oktoberfest"],
+        answer: "King's Day"
+      },
+      {
+        question: "What is the name of the famous Dutch painter known for his self-portraits?",
+        options: ["Rembrandt", "Vermeer", "Mondrian"],
+        answer: "Rembrandt"
+      },
+      {
+        question: "What is a popular Dutch sport?",
+        options: ["Soccer", "Cricket", "Baseball"],
+        answer: "Soccer"
+      },
+      {
+        question: "What traditional Dutch game is played on ice?",
+        options: ["Curling", "Korfball", "Speed skating"],
+        answer: "Speed skating"
+      },
+      {
+        question: "What is the Dutch term for \"cheese market\"?",
+        options: ["Kaasmarkt", "Kazen", "Kase"],
+        answer: "Kaasmarkt"
+      }
+    ]
+  },
+  {
+    image: 'https://intelligentediting.com/media/51588/Holland.jpg',
+    id: 4,
+    isLocked: true,
+    title: "Geography of the Netherlands",
+    questions: [
+      {
+        question: "Which body of water borders the Netherlands to the north?",
+        options: ["Atlantic Ocean", "North Sea", "Mediterranean Sea"],
+        answer: "North Sea"
+      },
+      {
+        question: "What is the highest point in the Netherlands?",
+        options: ["Vaalserberg", "Mount Snowdon", "Mount Everest"],
+        answer: "Vaalserberg"
+      },
+      {
+        question: "Which river runs through the Netherlands?",
+        options: ["Danube", "Rhine", "Seine"],
+        answer: "Rhine"
+      },
+      {
+        question: "Which city is known as the \"Venice of the North\"?",
+        options: ["Utrecht", "Amsterdam", "Rotterdam"],
+        answer: "Amsterdam"
+      },
+      {
+        question: "What is the main agricultural product of the Netherlands?",
+        options: ["Wheat", "Potatoes", "Flowers"],
+        answer: "Flowers"
+      },
+      {
+        question: "What is the climate like in the Netherlands?",
+        options: ["Tropical", "Temperate", "Arctic"],
+        answer: "Temperate"
+      },
+      {
+        question: "What is the area of the Netherlands?",
+        options: ["30,000 sq km", "41,543 sq km", "50,000 sq km"],
+        answer: "41,543 sq km"
+      },
+      {
+        question: "Which province is known for its windmills?",
+        options: ["Friesland", "Holland", "Zeeland"],
+        answer: "Holland"
+      },
+      {
+        question: "Which part of the Netherlands is below sea level?",
+        options: ["The North", "The West", "The East"],
+        answer: "The West"
+      },
+      {
+        question: "What is the official name of the Netherlands?",
+        options: ["Holland", "The Netherlands", "Dutch Republic"],
+        answer: "The Netherlands"
+      }
+    ]
+  },
+  {
+    image: 'https://www.planetware.com/wpimages/2020/02/netherlands-top-attractions-best-time-to-visit.jpg',
+    id: 5,
+    isLocked: true,
+    title: "Famous Landmarks and Attractions in the Netherlands",
+    questions: [
+      {
+        question: "What museum is famous for its collection of Van Gogh paintings?",
+        options: ["Rijksmuseum", "Van Gogh Museum", "Stedelijk Museum"],
+        answer: "Van Gogh Museum"
+      },
+      {
+        question: "What is the famous flower park in the Netherlands?",
+        options: ["Keukenhof", "Vondelpark", "Amsterdamse Bos"],
+        answer: "Keukenhof"
+      },
+      {
+        question: "Which historic building is the residence of the Dutch royal family?",
+        options: ["Rijksmuseum", "Noordeinde Palace", "Het Loo Palace"],
+        answer: "Noordeinde Palace"
+      },
+      {
+        question: "What is the name of the famous wooden shoes?",
+        options: ["Clogs", "Sabots", "Loafers"],
+        answer: "Clogs"
+      },
+      {
+        question: "Which city has a famous canal ring?",
+        options: ["Rotterdam", "Amsterdam", "Utrecht"],
+        answer: "Amsterdam"
+      },
+      {
+        question: "What landmark is a symbol of Amsterdam?",
+        options: ["The Rijksmuseum", "The Anne Frank House", "The Dam Square"],
+        answer: "The Dam Square"
+      },
+      {
+        question: "What is the name of the famous windmill village?",
+        options: ["Zaanse Schans", "Volendam", "Edam"],
+        answer: "Zaanse Schans"
+      },
+      {
+        question: "What is the largest aquarium in the Netherlands?",
+        options: ["Sea Life", "Oceanium", "Aquazoo"],
+        answer: "Oceanium"
+      },
+      {
+        question: "Which Dutch castle is known for its gardens?",
+        options: ["Muiderslot", "Kasteel de Haar", "Doorwerth Castle"],
+        answer: "Muiderslot"
+      },
+      {
+        question: "What is the name of the famous Dutch cheese market in Alkmaar?",
+        options: ["Kaasmarkt", "Boerenmarkt", "Fruitmarkt"],
+        answer: "Kaasmarkt"
+      }
+    ]
+  },
+  {
+    id: 6,
+    isLocked: true,
+    title: "Economy of the Netherlands",
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGHAHXhoMytyqOauBwzgwqv0TD55OiSOBhkA&s',
+    questions: [
+      {
+        question: "What sector is the main one in the economy of the Netherlands?",
+        options: ["Agriculture", "Tourism", "Technology"],
+        answer: "Agriculture"
+      },
+      {
+        question: "What currency is used in the Netherlands?",
+        options: ["Pound", "Euro", "Dollar"],
+        answer: "Euro"
+      },
+      {
+        question: "What product do the Netherlands export the most?",
+        options: ["Cheese", "Fruits", "Potatoes"],
+        answer: "Cheese"
+      },
+      {
+        question: "Which company is the largest beer producer in the Netherlands?",
+        options: ["Guinness", "Heineken", "Carlsberg"],
+        answer: "Heineken"
+      },
+      {
+        question: "Which of these companies is a well-known Dutch multinational corporation?",
+        options: ["Microsoft", "Unilever", "Toyota"],
+        answer: "Unilever"
+      },
+      {
+        question: "What product is the Netherlands famous for on the international market?",
+        options: ["Textiles", "Cauliflower", "Technology"],
+        answer: "Cauliflower"
+      },
+      {
+        question: "Which province of the Netherlands is the largest in agriculture?",
+        options: ["North Brabant", "Zeeland", "Groningen"],
+        answer: "Zeeland"
+      },
+      {
+        question: "What type of fishing is popular in the Netherlands?",
+        options: ["Tuna fishing", "Herring fishing", "Salmon fishing"],
+        answer: "Herring fishing"
+      },
+      {
+        question: "Which sector has the most foreign investment in the Netherlands?",
+        options: ["Transportation", "Technology", "Agriculture"],
+        answer: "Technology"
+      },
+      {
+        question: "Which port is key for international trade in the Netherlands?",
+        options: ["Amsterdam", "The Hague", "Rotterdam"],
+        answer: "Rotterdam"
+      }
+    ]
+  },
+  {
+    id: 7,
+    isLocked: true,
+    title: "Nature of the Netherlands",
+    image: 'https://scx2.b-cdn.net/gfx/news/hires/2022/netherlands.jpg',
+    questions: [
+      {
+        question: "Which of these animals is a symbol of the Netherlands?",
+        options: ["Horse", "Dog", "Cat"],
+        answer: "Horse"
+      },
+      {
+        question: "Which of these lakes is the largest in the Netherlands?",
+        options: ["Flevopolder", "Groningen", "Oslo"],
+        answer: "Flevopolder"
+      },
+      {
+        question: "Which of these natural areas is part of the Netherlands?",
+        options: ["Dunes", "Mountains", "Forests"],
+        answer: "Dunes"
+      },
+      {
+        question: "Which of these nature reserves are in the Netherlands?",
+        options: ["De Hoge Veluwe National Park", "Teide National Park", "Lapland National Park"],
+        answer: "De Hoge Veluwe National Park"
+      },
+      {
+        question: "Which of these rivers flows through the Netherlands?",
+        options: ["Rhine", "IJssel", "Danube"],
+        answer: "Rhine"
+      },
+      {
+        question: "Which flower is traditional for the Netherlands?",
+        options: ["Rose", "Tulip", "Lily"],
+        answer: "Tulip"
+      },
+      {
+        question: "Which animal is the largest in the Netherlands?",
+        options: ["Cow", "Elephant", "Horse"],
+        answer: "Elephant"
+      },
+      {
+        question: "What type of landscape is characteristic of the Netherlands?",
+        options: ["Mountains", "Plains", "Caves"],
+        answer: "Plains"
+      },
+      {
+        question: "Which of these birds is a symbol of the Netherlands?",
+        options: ["Swallow", "Eagle", "Tit"],
+        answer: "Swallow"
+      },
+      {
+        question: "Which of these plants is characteristic of the Netherlands?",
+        options: ["Beech", "Lavender", "Melissa"],
+        answer: "Lavender"
+      }
+    ]
+  }
+];
